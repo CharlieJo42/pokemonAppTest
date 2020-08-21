348a35976783f1e4d59637423c15c88abc48ce43
Charlie Jo <charliejames1652@gmail.com>
2020-08-21 05:49:55 -0400

    saved pokemon.js

--- poke-test-app/src/App.js
+++ poke-test-app/src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 
 function App() {
   const [pokemonData, setPokemonData] = useState([])
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon'
   return (
     <div >
       
