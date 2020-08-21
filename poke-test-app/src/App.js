37683baf89b780fb3f740fa83190fefc63f1b31e
Charlie Jo <charliejames1652@gmail.com>
2020-08-21 06:17:21 -0400

    updated card.js

--- poke-test-app/src/App.js
+++ poke-test-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect} from 'react';
-import { getAllPokemon } from './services/pokemon';
+import { getAllPokemon, getPokemon } from './services/pokemon';
+import Card from './components/Card'
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,7 +14,6 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       let response = await getAllPokemon(initialUrl);
-      console.log(response);
       setNextUrl(response.next);
       setPrevUrl(response.previous);
       await loadingPokemon(response.results);
@@ -23,15 +23,29 @@ function App() {
   }, [])
 
   const loadingPokemon = async (data) => {
-    let _pokemon = await Promise.all(data.map(async pokemon => {
-      let pokemonRecord = await getPokemon(pokmeon)
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url);
+      return pokemonRecord;
     }))
-  }
+
+    setPokemonData(_pokemonData);
+
+
+  };
+
+
   return (
     <div >
-      { loading ? <h1>Loading...</h1> : (
-        <h1>Data is fetched</h1>
-      )}
+      { 
+      loading ? <h1>Loading...</h1> : (
+      <> 
+        <div className="grid-container">
+          {pokemonData.map((pokemon, i) => {
+            return <Card key={i} pokemon={pokemon}/>
+          })}
+        </div>
+      </>)
+      }
     </div>
   );
 }
---
+++ poke-test-app/src/components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Card({ pokemon }) {
+    return(
+       <div className="Card">
+           <div className="Card__img">
+               <div className="Card__Name">
+
+                   <div className="Card__types">
+
+                       <div className="Card__info">
+                           
+                       </div>
+                   </div>
+               </div>
+               </div> 
+
+       </div>
+    )
+}
