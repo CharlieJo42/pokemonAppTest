1e33fcf2d57ca2d08d41b5886a96f8b7d5811178
Charlie Jo <charliejames1652@gmail.com>
2020-08-21 05:54:30 -0400

    successful console log test

--- poke-test-app/src/App.js
+++ poke-test-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect} from 'react';
+import { getAllPokemon } from './services/pokemon';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,9 +9,22 @@ function App() {
   const [prevUrl, setPrevUrl] = useState('');
   const [loading, setLoading] = useState(true);
   const initialUrl = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl);
+      console.log(response);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      setLoading(false);
+    }
+    fetchData();
+  }, [])
   return (
     <div >
-      
+      { loading ? <h1>Loading...</h1> : (
+        <h1>Data is fetched</h1>
+      )}
     </div>
   );
 }
